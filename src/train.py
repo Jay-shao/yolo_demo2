bec3d339ca3c80b924c081d2c552473f2dc02afe
JayShao <jayabc31@gmail.com>
2026-08-31 09:09:46 +0800

    complete 100 epochs training

---
+++ src/train.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+
+
+model = YOLO("yolo11n.pt")
+
+
+model.train(
+    data="dataset/data.yaml",
+    epochs=100,
+    imgsz=640,
+    batch=8,
+    device=0,
+    workers=4,
+
+    project="runs",
+    name="yolo11n_4classes",
+
+    pretrained=True,
+    patience=30,
+    plots=True
+)
